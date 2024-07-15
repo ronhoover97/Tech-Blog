a203b2937427759066d311377ee12f25062be92d
97rhoover <97rhoover@gmail.com>
2024-07-15 14:05:26 -0400

    Updated handlebars, updated ReadMe

---
+++ README.md
@@ -0,0 +1,39 @@
+# Tech-Blog
+
+## Description
+
+An application that enables users to "Blog" about tech.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Video](#video)
+- [Badges](#badges)
+- [Contributing](#contributing)
+- [Tests](#tests)
+
+## Installation
+
+Clone this repo and enjoy!
+
+## Tutorial Video
+
+If there is a video tutorial available for this application, you can watch that video here.
+
+## Credits
+
+N/A
+
+## Badges
+
+N/A
+
+## Contributing
+
+I am not accepting contributers for this project at the moment.
+
+## Tests
+
+N/A
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render("post", {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
